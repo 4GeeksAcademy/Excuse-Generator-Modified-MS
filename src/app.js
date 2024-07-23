bfed59b23302363335e6b08924d5878d560a1eed
monalisasingh87 <monalisa.singh87@gmail.com>
2024-07-23 16:02:50 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let generateExcuse = () => {
+    let whoPart = who[Math.floor(Math.random() * who.length)];
+    let actionPart = action[Math.floor(Math.random() * action.length)];
+    let whatPart = what[Math.floor(Math.random() * what.length)];
+    let whenPart = when[Math.floor(Math.random() * when.length)];
+
+    return `${whoPart} ${actionPart} ${whatPart} ${whenPart}`;
+  };
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
